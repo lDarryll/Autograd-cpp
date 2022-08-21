599b19a1f323552d2479a08a0a4f21c6c97a50dc
shengzeng <sheng.zeng@aitronx.com>
2022-08-21 23:49:38 +0800

    Multiply

--- src/main.cpp
+++ src/main.cpp
@@ -72,18 +72,81 @@ public:
     float gradient_ = 0;
 };
 
+
+class Scalar : public Expression
+{
+    public:
+        Scalar(float value)
+        {
+            container_.reset(new ScalarContainer(value));
+        }
+
+        float gradient() const 
+        {
+            return dynamic_pointer_cast<ScalarContainer>(container_)->gradient_;
+            // return ((ScalarContainer*)container_.get())->gradient_;
+        }
+};
+
+class MultiplyContainer : public Container
+{
+public:
+    MultiplyContainer(const Expression& left, const Expression& right)
+    {
+        left_value_ = left.container_;
+        right_value_ = right.container_;
+    }
+    //返回表达是具体是什么类型，或者说什么名称
+    virtual const char *type() override
+    {
+        return "Multiply";
+    }
+
+    // 具体的forward的实现过程
+    virtual float forward() override
+    {
+        return left_value_->forward() * right_value_->forward();
+    }
+
+    // 具体的backward的实现
+    virtual void backward(float gradient) override
+    {
+        left_value_->backward(gradient * right_value_->forward());
+        right_value_->backward(gradient * left_value_->forward());
+    }  
+
+    shared_ptr<Container> left_value_;
+    shared_ptr<Container> right_value_;
+};
+
+class Multiply : public Expression 
+{
+    public:
+        Multiply(const Expression& left, const Expression& right)
+        {
+            container_.reset(new MultiplyContainer(left, right));
+        }
+};
+
 int main()
 {
     // 1. 实现计算图的统计
     // 2. 实现过程，和四则运算类似，和普通表达式类似
     // 3. 实现forward前向计算，和backward反向求导
-
     // a 是一个通过调整得到的值，通过梯度方向调整，最终逼近sqrt(x)
     // x 是一个常量，需要求解的对象
 
     float x = 9;
-    Scalar a(x / 2.0f);
-    auto loss = (a.power() - x).power();
+    Scalar a(3.0f);
+    // a uto loss = (a.power() - x).power();
+
+    // cout << a.forward() <<endl;
+    auto exp = Multiply(a, Scalar(5.0f));
+    cout << exp.forward() <<endl;
+    exp.backward();
+    cout << a.gradient() << endl;
+    // cout << a.gradient() << endl;
+
 
     return 0;
 }
